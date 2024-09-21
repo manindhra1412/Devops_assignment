0054b42fd8c4a2a13b24d86de572760e10e4a782
Manindhra Goud <manindhra1412@gmail.com>
2024-09-21 14:59:26 +0530

    Manindhra

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverse(int arr[], int n)
+{
+    for (int i = 0; i < (n / 2); i++)
+    {
+        swap(arr[i], arr[n - i - 1]);
+    }
+}
+
+int main()
+{
+    int n = 6;
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    reverse(arr, n);
+    for (auto i : arr)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
